6a90da00a36e646c691bebb26d56cda3daa9c82c
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-04-22 11:15:20 +0300

    Eski haline getirildi

--- blocks/depo_yonetimi/lang/en/block_depo_yonetimi.php
+++ blocks/depo_yonetimi/lang/en/block_depo_yonetimi.php
@@ -1,10 +1,8 @@
 <?php
 
 $string['pluginname'] = 'Lojix Depo Paneli';
-+ $string['depo_yonetimi:addinstance'] = 'Yeni depo paneli bloğu ekle';
-+ $string['depo_yonetimi:myaddinstance'] = 'Kendi sayfana depo paneli bloğu ekle';
+$string['depo_paneli:addinstance'] = 'Yeni depo paneli bloğu ekle';
+$string['depo_paneli:myaddinstance'] = 'Kendi sayfana depo paneli bloğu ekle';
 $string['depo_yonetimi:viewall'] = 'Tüm depoları görme izni';
 $string['depo_yonetimi:viewown'] = 'Sadece atanmış depoyu görme izni';
 
-$string['productname'] = 'Product Name';
-$string['quantity'] = 'Quantity';
