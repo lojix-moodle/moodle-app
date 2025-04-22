bab38c4c183affd22755961a1c9788fc57752e88
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-04-22 16:05:55 +0300

    update

--- blocks/depo_yonetimi/actions/urun_ekle.php
+++ blocks/depo_yonetimi/actions/urun_ekle.php
@@ -1,27 +1,47 @@
 <?php
-
 require('../../config.php');
-require_once($CFG->dirroot.'/blocks/depo_yonetimi/classes/form/urun_form.php');
 
 $depoid = required_param('depoid', PARAM_INT);
-$url = new moodle_url('/blocks/depo_yonetimi/actions/urun_ekle.php', ['depoid' => $depoid]);
+$actionurl = new moodle_url('/blocks/depo_yonetimi/actions/urun_ekle.php', ['depoid' => $depoid]);
 
-// 💥 EKSİK OLAN KISIM: PAGE AYARLARI
-$PAGE->set_url($url);
-$PAGE->set_context(context_system::instance()); // veya uygun context
-$PAGE->set_title('Ürün Ekle');
-$PAGE->set_heading('Ürün Ekle');
+// Sahte veri deposu (örnek amaçlı)
+$urunler_json = $CFG->dataroot . '/urunler.json';
+if (!file_exists($urunler_json)) {
+    file_put_contents($urunler_json, json_encode([]));
+}
+$urunler = json_decode(file_get_contents($urunler_json), true);
 
-$mform = new urun_form($url->out(false));
-$mform->set_data(['depoid' => $depoid]);
+// Form gönderildi mi?
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = required_param('name', PARAM_TEXT);
+    $adet = required_param('adet', PARAM_INT);
 
-if ($mform->is_cancelled()) {
-    redirect(new moodle_url('/my', ['depo' => $depoid]));
-} else if ($data = $mform->get_data()) {
-    // JSON dosyası veya DB işlemleri yapılır
+    if (!isset($urunler[$depoid])) {
+        $urunler[$depoid] = [];
+    }
+
+    $urunler[$depoid][] = [
+        'name' => $name,
+        'adet' => $adet,
+    ];
+
+    file_put_contents($urunler_json, json_encode($urunler));
     redirect(new moodle_url('/my', ['depo' => $depoid]), 'Ürün eklendi');
 }
 
+// Sayfa çıktısı
 echo $OUTPUT->header();
-$mform->display();
+echo html_writer::tag('h2', 'Ürün Ekle');
+
+echo '<form method="POST">';
+echo '<label>Ürün Adı:</label><br>';
+echo '<input type="text" name="name" required><br><br>';
+
+echo '<label>Adet:</label><br>';
+echo '<input type="number" name="adet" required><br><br>';
+
+echo '<input type="submit" value="Ekle">';
+echo '</form>';
+
+echo html_writer::link(new moodle_url('/my', ['depo' => $depoid]), '← Geri Dön');
 echo $OUTPUT->footer();
