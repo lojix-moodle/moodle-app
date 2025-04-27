2b557c536d41e12700892bdb0761d4299b4e29e2
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-04-27 17:20:32 +0300

    depo ekle butonu update

--- blocks/depo_yonetimi/actions/depo_ekle.php
+++ blocks/depo_yonetimi/actions/depo_ekle.php
@@ -1,22 +1,14 @@
 <?php
-
-require('../../../../config.php'); // config.php'ye doğru yolu ver
+require('../../config.php');
 require_login();
 global $DB, $PAGE, $OUTPUT;
 
-// Sayfa ayarları
 $PAGE->set_url(new moodle_url('/blocks/depo_yonetimi/actions/depo_ekle.php'));
-
 $PAGE->set_context(context_system::instance());
 $PAGE->set_title('Depo Ekle');
 $PAGE->set_heading('Depo Ekle');
 
-global $DB, $OUTPUT;
-
-// Form gönderildiyse
-if (optional_param('submit', null, PARAM_TEXT)) {
-    require_sesskey(); // Güvenlik için
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && confirm_sesskey()) {
     $name = required_param('name', PARAM_TEXT);
 
     $depo = new stdClass();
@@ -27,20 +19,16 @@ if (optional_param('submit', null, PARAM_TEXT)) {
     redirect(new moodle_url('/my'), 'Depo başarıyla eklendi.');
 }
 
-// Sayfa başı
 echo $OUTPUT->header();
+echo html_writer::tag('h2', 'Yeni Depo Ekle');
 
-// Form
-echo '<h2>Yeni Depo Ekle</h2>';
 echo '<form method="POST">';
 echo '<label>Depo Adı:</label><br>';
 echo '<input type="text" name="name" required><br><br>';
 echo '<input type="hidden" name="sesskey" value="' . sesskey() . '">';
-echo '<input type="submit" name="submit" value="Depo Ekle" class="btn btn-primary">';
+echo '<input type="submit" value="Ekle">';
 echo '</form>';
 
-echo '<br><a href="' . new moodle_url('/my') . '">← Geri Dön</a>';
-
-// Sayfa sonu
+echo html_writer::link(new moodle_url('/my'), '← Geri Dön');
 echo $OUTPUT->footer();
 ?>
