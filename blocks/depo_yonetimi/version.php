2b585c3a8c7ed423af60f5c40bf842de5f63fe74
ilaydakbskl <ilaydakabasakal8@gmail.com>
2025-05-02 13:55:43 +0300

    depo sil düzenle

--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_depo_yonetimi';
-$plugin->version = 2025042193;
+$plugin->version = 2025042194;
 $plugin->requires = 2022112800; // Moodle 4.x
 
 
