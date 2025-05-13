7a8d05df0c8074d0fc923bff2e2907c69a3c19a8
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-13 22:27:20 +0300

    dashboard silindi eklenenler

--- blocks/depo_yonetimi/lang/en/block_depo_yonetimi.php
+++ blocks/depo_yonetimi/lang/en/block_depo_yonetimi.php
@@ -1,44 +1,11 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Language strings for Depo Yonetimi
- *
- * @package    block_depo_yonetimi
- * @copyright  2025 Your Name
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+$string['pluginname'] = 'Lojix Depo Paneli';
+$string['depo_paneli:addinstance'] = 'Yeni depo paneli bloğu ekle';
+$string['depo_paneli:myaddinstance'] = 'Kendi sayfana depo paneli bloğu ekle';
+$string['depo_yonetimi:viewall'] = 'Tüm depoları görme izni';
+$string['depo_yonetimi:viewown'] = 'Sadece atanmış depoyu görme izni';
 
-$string['pluginname'] = 'Depo Yönetimi';
-$string['depo_yonetimi:addinstance'] = 'Yeni bir Depo Yönetimi bloğu ekle';
-$string['depo_yonetimi:myaddinstance'] = 'Dashboard\'a yeni bir Depo Yönetimi bloğu ekle';
-$string['depo_yonetimi:view'] = 'Depo Yönetimi\'ni görüntüle';
-$string['depo_yonetimi:manage'] = 'Depo Yönetimi\'ni yönet';
-$string['viewdashboard'] = 'Dashboard\'a Git';
-$string['dashboard'] = 'Depo Yönetimi Dashboard';
-$string['daily_summary'] = 'Günlük Özet';
-$string['weekly_summary'] = 'Haftalık Özet';
-$string['monthly_summary'] = 'Aylık Özet';
-$string['entries'] = 'Giriş';
-$string['exits'] = 'Çıkış';
-$string['orders'] = 'Sipariş';
-$string['net_change'] = 'Net Değişim';
-$string['stock_levels'] = 'Stok Seviyeleri';
-$string['pending_orders'] = 'Bekleyen Siparişler';
-$string['critical_stock_alerts'] = 'Kritik Stok Uyarıları';
-$string['recent_activities'] = 'Son Hareketler';
-$string['no_data'] = 'Veri bulunamadı';
-$string['no_alerts'] = 'Kritik stok uyarısı bulunmamaktadır';
+
+$string['productname'] = 'Product Name';
+$string['quantity'] = 'Quantity';
--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -3,8 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_depo_yonetimi';
-$plugin->version = 2025042185;
+$plugin->version = 2025042180;
 $plugin->requires = 2022112800; // Moodle 4.x
-
 
 
