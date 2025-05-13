73e3ebefc9630b8591d499c8611f44b5e9046c6e
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-13 23:59:43 +0300

    versiyon arttı

--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_depo_yonetimi';
-$plugin->version = 2025042183;
+$plugin->version = 2025042185;
 $plugin->requires = 2022112800; // Moodle 4.x
 
 
