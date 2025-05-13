16466d7eff3483e3e8f2d8fd81c6d4124e0d93ca
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-13 23:41:23 +0300

    kategori edüzenle dosyası düzenlendi

--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_depo_yonetimi';
-$plugin->version = 2025042182;
+$plugin->version = 2025042183;
 $plugin->requires = 2022112800; // Moodle 4.x
 
 
