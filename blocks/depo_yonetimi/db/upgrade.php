9b0f76ed4693a2ada7a7611b9de15e4ca0d4a587
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-14 15:30:21 +0300

    update

---
+++ blocks/depo_yonetimi/db/upgrade.php
@@ -0,0 +1,27 @@
+<?php
+
+function xmldb_block_depo_yonetimi_upgrade($oldversion) {
+    global $DB;
+
+    $dbman = $DB->get_manager();
+
+    if ($oldversion < 2025042190) {
+
+        $table = new xmldb_table('block_depo_yonetimi_kategoriler');
+
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('name', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
+
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        upgrade_block_savepoint(true, 2025042202, 'depo_yonetimi');
+    }
+
+    return true;
+}
--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -5,5 +5,3 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'block_depo_yonetimi';
 $plugin->version = 2025042189;
 $plugin->requires = 2022112800; // Moodle 4.x
-
-
