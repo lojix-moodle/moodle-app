f2a28c0353916422defa719603496f417238575e
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-14 20:39:56 +0300

    depo düzenleme update

--- blocks/depo_yonetimi/db/upgrade.php
+++ blocks/depo_yonetimi/db/upgrade.php
@@ -5,18 +5,18 @@ function xmldb_block_depo_yonetimi_upgrade($oldversion) {
 
     $dbman = $DB->get_manager();
 
-//    if ($oldversion < 2025042204) {
-//        $table = new xmldb_table('block_depo_yonetimi_urunler');
-//
-//        $field = new xmldb_field('kategoriid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
-//
-//        if (!$dbman->field_exists($table, $field)) {
-//            $dbman->add_field($table, $field);
-//        }
-//
-//        // Güncelleme noktasını kaydet
-//        upgrade_block_savepoint(true, 2025051401, 'depo_yonetimi');
-//    }
+    if ($oldversion < 2025051422) {
+        $table = new xmldb_table('block_depo_yonetimi_depolar');
+
+        $field = new xmldb_field('sorumluid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
+
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Güncelleme noktasını kaydet
+        upgrade_block_savepoint(true, 2025051422, 'depo_yonetimi');
+    }
 
 
     return true;
