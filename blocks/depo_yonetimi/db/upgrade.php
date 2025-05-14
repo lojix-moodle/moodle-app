57c783c1c8e693a0314a72f34451d86d1719a47b
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-14 20:27:40 +0300

    version update

--- blocks/depo_yonetimi/db/upgrade.php
+++ blocks/depo_yonetimi/db/upgrade.php
@@ -5,23 +5,18 @@ function xmldb_block_depo_yonetimi_upgrade($oldversion) {
 
     $dbman = $DB->get_manager();
 
-    if ($oldversion < 2025051417) {
-        $table = new xmldb_table('block_depo_yonetimi_depolar');
-
-        // Eğer tablo varsa işlem yap
-        if ($dbman->table_exists($table)) {
-            $field = new xmldb_field('sorumluid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0', 'name');
-
-            // Eğer sütun yoksa ekle
-            if (!$dbman->field_exists($table, $field)) {
-                $dbman->add_field($table, $field);
-            }
-        }
-
-        // Sürüm numarasını güncelle
-        upgrade_block_savepoint(true, 2025051417, 'depo_yonetimi');
-
-    }
+//    if ($oldversion < 2025042204) {
+//        $table = new xmldb_table('block_depo_yonetimi_urunler');
+//
+//        $field = new xmldb_field('kategoriid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
+//
+//        if (!$dbman->field_exists($table, $field)) {
+//            $dbman->add_field($table, $field);
+//        }
+//
+//        // Güncelleme noktasını kaydet
+//        upgrade_block_savepoint(true, 2025051401, 'depo_yonetimi');
+//    }
 
 
     return true;
