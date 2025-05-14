d8d51eccd4fc80893b3fcce5a8101c8fec85776d
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-14 18:17:14 +0300

    anasayfa update

--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -3,5 +3,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_depo_yonetimi';
-$plugin->version = 2025051408;
+$plugin->version = 2025051409;
 $plugin->requires = 2022112800; // Moodle 4.x
