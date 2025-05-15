788c6f058910a442581f5a72bb0faccdbfc91374
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-15 13:07:13 +0300

    dil dosyalrı güncellendi

--- blocks/depo_yonetimi/lang/en/block_depo_yonetimi.php
+++ blocks/depo_yonetimi/lang/en/block_depo_yonetimi.php
@@ -9,3 +9,18 @@ $string['depo_yonetimi:viewown'] = 'Sadece atanmış depoyu görme izni';
 
 $string['productname'] = 'Product Name';
 $string['quantity'] = 'Quantity';
+
+$string['depo_ekle'] = 'Depo Ekle';
+$string['depo_adi'] = 'Depo Adı';
+$string['depo_sorumlusu'] = 'Depo Sorumlusu';
+$string['depo_ekle_success'] = 'Depo başarıyla eklendi.';
+$string['depo_ekle_error'] = 'Depo eklenirken hata oluştu.';
+$string['depo_guncelle'] = 'Depo Güncelle';
+$string['depo_guncelle_success'] = 'Depo başarıyla güncellendi.';
+$string['depo_guncelle_error'] = 'Depo güncellenirken hata oluştu.';
+$string['depo_sil'] = 'Depo Sil';
+$string['depo_sil_success'] = 'Depo başarıyla silindi.';
+$string['depo_sil_error'] = 'Depo silinirken hata oluştu.';
+$string['depo_listele'] = 'Depo Listele';
+$string['depo_listele_success'] = 'Depolar başarıyla listelendi.';
+$string['depo_listele_error'] = 'Depolar listelenirken hata oluştu.';
