1839a7790adf767e06ee6abbd25e8928c9386d89
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-18 00:48:39 +0300

    login page update

--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -3,6 +3,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_depo_yonetimi';
-$plugin->version = 2025051776;
+$plugin->version = 2025051777;
 
 $plugin->requires = 2022112800; // Moodle 4.x
