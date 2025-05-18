fa7fa7780d9857230679822de3e19f46dd7d4e51
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-18 15:06:12 +0300

    Fix transaction handling in depo_ekle.php and update version number

--- blocks/depo_yonetimi/actions/depo_ekle.php
+++ blocks/depo_yonetimi/actions/depo_ekle.php
@@ -272,8 +272,8 @@ if (isset($_POST['submit']) || (isset($_POST['name']) && isset($_POST['sorumluid
         $newdepo->timemodified = time();
         $newdepo->createdby = $USER->id;
 
+        $transaction = $DB->start_delegated_transaction();
         try {
-            $DB->start_delegated_transaction();
             $depoid = $DB->insert_record('block_depo_yonetimi_depolar', $newdepo);
 
             $log = new stdClass();
@@ -284,13 +284,11 @@ if (isset($_POST['submit']) || (isset($_POST['name']) && isset($_POST['sorumluid
             $log->timecreated = time();
             $DB->insert_record('block_depo_yonetimi_logs', $log);
 
-            $DB->commit_delegated_transaction();
+            $DB->commit_delegated_transaction($transaction);
 
             redirect(new moodle_url('/blocks/depo_yonetimi/index.php'), 'Depo başarıyla eklendi.', null, \core\output\notification::NOTIFY_SUCCESS);
         } catch (Exception $e) {
-            // Hatalı çağrı: $DB->rollback_delegated_transaction();
-            // Düzeltilmiş çağrı:
-            $DB->rollback_delegated_transaction($e);
+            $DB->rollback_delegated_transaction($transaction);
             redirect(new moodle_url('/blocks/depo_yonetimi/actions/depo_ekle.php'), 'Depo eklenirken bir hata oluştu: ' . $e->getMessage(), null, \core\output\notification::NOTIFY_ERROR);
         }
     } else {
