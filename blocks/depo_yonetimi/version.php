cec075ce42f9eebece42b37959f8dc274025c37e
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-20 16:17:01 +0300

    Refactor login form to enhance input handling, improve button functionality, and streamline submission process

--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -4,6 +4,6 @@ defined('MOODLE_INTERNAL') || die();
 
 
 $plugin->component = 'block_depo_yonetimi';
-$plugin->version = 2025052016; // Versiyon numarasını artırdık
+$plugin->version = 2025052017; // Versiyon numarasını artırdık
 
 $plugin->requires = 2022112800; // Moodle 4.x
