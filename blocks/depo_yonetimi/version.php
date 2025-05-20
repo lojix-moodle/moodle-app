fa76dea13e47ccfb006188881f91de5817091b84
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-20 12:16:49 +0300

    Remove minimum stock level field from product addition form and update version number

--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -4,6 +4,6 @@ defined('MOODLE_INTERNAL') || die();
 
 
 $plugin->component = 'block_depo_yonetimi';
-$plugin->version = 2025052007; // Versiyon numarasını artırdık
+$plugin->version = 2025052009; // Versiyon numarasını artırdık
 
 $plugin->requires = 2022112800; // Moodle 4.x
