1a9ac133273424edf9ab3179aa321ae9f5ecb30d
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-20 21:20:48 +0300

    feat: update database schema for block_depo_yonetimi with new fields and version increment

--- blocks/depo_yonetimi/db/upgrade.php
+++ blocks/depo_yonetimi/db/upgrade.php
@@ -5,15 +5,23 @@ function xmldb_block_depo_yonetimi_upgrade($oldversion) {
 
     $dbman = $DB->get_manager();
 
-    if ($oldversion < 2025051947) {
+    if ($oldversion < 2025051948) {
         $table = new xmldb_table('block_depo_yonetimi_talepler');
 
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('depoid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('urunid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
         $table->add_field('renk', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, null);
         $table->add_field('beden', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('durum', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, 0);
+        $table->add_field('adet', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('durum', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, );
+
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
 
         // Güncelleme noktasını kaydet
-        upgrade_block_savepoint(true, 2025051947, 'depo_yonetimi');
+        upgrade_block_savepoint(true, 2025051948, 'depo_yonetimi');
     }
 
     return true;
