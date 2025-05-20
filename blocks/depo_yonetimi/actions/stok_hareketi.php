7ce3c50da6ca57ab774474721acd3438828abd3d
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-05-20 18:51:22 +0300

    fix: update version number and improve stock movement table check

--- blocks/depo_yonetimi/actions/stok_hareketi.php
+++ blocks/depo_yonetimi/actions/stok_hareketi.php
@@ -37,7 +37,7 @@ $min_stok = isset($urun->min_stok_seviyesi) ? $urun->min_stok_seviyesi : 0;
 
 // Stok hareketleri tablosu kontrolü - table_exists hatasını önlemek için get_manager() kullan
 $table = new xmldb_table('block_depo_yonetimi_stok_hareketleri');
-$stok_hareketi_tablo_var = $DB->get_manager()->table_exists('block_depo_yonetimi_stok_hareketleri');
+$stok_hareketi_tablo_var = $DB->get_records("block_depo_yonetimi_stok_hareketleri", ['urunid' => $urunid]);
 // Form gönderildiğinde
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require_sesskey();
