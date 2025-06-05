f90b706217cd2eb25b0e469ab7f1a659b9b22d3c
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-06-05 11:47:38 +0300

    feat: update database schema for stock movement with additional fields and foreign key constraints

--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -4,6 +4,6 @@ defined('MOODLE_INTERNAL') || die();
 
 
 $plugin->component = 'block_depo_yonetimi';
-$plugin->version = 2025060510; // Versiyon numarasını artırdık
+$plugin->version = 2025060511; // Versiyon numarasını artırdık
 
 $plugin->requires = 2022112800; // Moodle 4.x
