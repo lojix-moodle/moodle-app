48c7faf6efaccb25a78d6a214c5e7e4147da5fdb
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-06-07 13:45:37 +0300

    feat: update stock action link titles and add new stock movement link in block_depo_yonetimi.php, increment version number

--- blocks/depo_yonetimi/block_depo_yonetimi.php
+++ blocks/depo_yonetimi/block_depo_yonetimi.php
@@ -254,9 +254,15 @@ class="btn btn-info">
                 <a href="' . new moodle_url('/blocks/depo_yonetimi/actions/stok_list.php', [
                             'depoid' => $depoid,
                             'urunid' => $urun->id
-                        ]) . '" class="btn btn-sm btn-outline-success" data-bs-toggle="tooltip" title="Stok Hareketi">
+                        ]) . '" class="btn btn-sm btn-outline-success" data-bs-toggle="tooltip" title="Stok Listesi">
     <i class="fas fa-cube"></i>
 </a>
+                     <a href="' . new moodle_url('/blocks/depo_yonetimi/actions/stok_hareketleri.php', [
+                            'depoid' => $depoid,
+                            'urunid' => $urun->id
+                        ]) . '" class="btn btn-sm btn-outline-secondary" data-bs-toggle="tooltip" title="Stok Hareketleri">
+            <i class="fas fa-history"></i>
+        </a>
                   <a href="' . new moodle_url('/blocks/depo_yonetimi/actions/urun_duzenle.php', [
                             'depoid' => $depoid,
                             'urunid' => $urun->id
