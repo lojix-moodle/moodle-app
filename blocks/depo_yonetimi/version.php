f379381e9620b93c86c1caa8f24199620cf8e0b6
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-06-08 15:00:51 +0300

    feat: enhance homepage design with modern layout, improved JavaScript functionality, and critical stock alerts

--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -4,6 +4,8 @@ defined('MOODLE_INTERNAL') || die();
 
 
 $plugin->component = 'block_depo_yonetimi';
-$plugin->version = 2025060811; // Versiyon numarasını artırdık
+$plugin->version = 2025060812; // Versiyon numarasını artırdık
 
 $plugin->requires = 2022112800; // Moodle 4.x
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
