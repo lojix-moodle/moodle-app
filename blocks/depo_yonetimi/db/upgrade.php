b31255e291c05ab7a72c5256299028499e2edbff
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-06-09 22:14:41 +0300

    fix: update upgrade script to add 'barkod' field and increment version number

--- blocks/depo_yonetimi/db/upgrade.php
+++ blocks/depo_yonetimi/db/upgrade.php
@@ -1,4 +1,28 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Depo Yönetimi blok yükseltme kodları
+ *
+ * @package    block_depo_yonetimi
+ * @copyright  2024 Depo Yönetimi Geliştirme Ekibi
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
 
 function xmldb_block_depo_yonetimi_upgrade($oldversion) {
     global $DB;
@@ -6,6 +30,7 @@ function xmldb_block_depo_yonetimi_upgrade($oldversion) {
     $dbman = $DB->get_manager();
 
     if ($oldversion < 2025060561) {
+        // Talep tablosu oluşturma
         $table = new xmldb_table('block_depo_yonetimi_talepler');
 
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
@@ -14,7 +39,7 @@ function xmldb_block_depo_yonetimi_upgrade($oldversion) {
         $table->add_field('renk', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, null);
         $table->add_field('beden', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, null);
         $table->add_field('adet', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('durum', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, );
+        $table->add_field('durum', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, null); // Virgülü kaldırdım
 
         $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
 
@@ -22,9 +47,7 @@ function xmldb_block_depo_yonetimi_upgrade($oldversion) {
             $dbman->create_table($table);
         }
 
-
-
-        // DÜZELTME: Doğru tabloyu hedefle
+        // Ürünler tablosuna raf ve bölüm alanları ekleme
         $urunler_table = new xmldb_table('block_depo_yonetimi_urunler');
 
         $field_raf = new xmldb_field('raf', XMLDB_TYPE_CHAR, '100', null, null, null, null, 'varyasyonlar');
@@ -37,9 +60,23 @@ function xmldb_block_depo_yonetimi_upgrade($oldversion) {
             $dbman->add_field($urunler_table, $field_bolum);
         }
 
-
         // Güncelleme noktasını kaydet
         upgrade_block_savepoint(true, 2025060561, 'depo_yonetimi');
+    }
+
+    if ($oldversion < 2025060917) {
+        // Ürünler tablosuna barkod alanı ekle
+        $urunler_table = new xmldb_table('block_depo_yonetimi_urunler');
+
+        $field_barkod = new xmldb_field('barkod', XMLDB_TYPE_CHAR, '100', null, null, null, null, 'bolum');
+        if (!$dbman->field_exists($urunler_table, $field_barkod)) {
+            $dbman->add_field($urunler_table, $field_barkod);
+        }
+
+
+
+        // Güncelleme noktasını kaydet
+        upgrade_block_savepoint(true, 2025060917, 'depo_yonetimi');
     }
 
     return true;
