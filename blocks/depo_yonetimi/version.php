7a6cbbb2bca1fa29407ec2c258c45ee99879ff27
Saniye EKİNCİ <ekincisaniye52@gmail.com>
2025-06-10 21:03:15 +0300

    refactor: streamline variant display logic in urun_duzenle.php; remove pagination and update version number

--- blocks/depo_yonetimi/version.php
+++ blocks/depo_yonetimi/version.php
@@ -4,6 +4,6 @@ defined('MOODLE_INTERNAL') || die();
 
 
 $plugin->component = 'block_depo_yonetimi';
-$plugin->version = 2025061025; // Versiyon numarasını artırdık
+$plugin->version = 2025061026; // Versiyon numarasını artırdık
 
 $plugin->requires = 2022112800; // Moodle 4.x
